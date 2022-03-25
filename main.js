6c6dcb987a28953e383c608cc4084fbfa6b267ec
IsahCS <isabelcordeiro1230@gmail.com>
2022-03-25 20:25:05 -0300

    up

---
+++ main.js
@@ -0,0 +1,122 @@
+const urlApi = `http://localhost:8080/api/breakfast/`;
+const table = document.querySelector("#tabela-breakfast");
+const initialTable = document.querySelector("#cabecalho");
+const formulario = document.querySelector("#formulario");
+
+const breakfastRouteUrl = (route = "") => `${urlApi}${route}`; 
+
+const headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+};
+
+const fetchBreakfast = async() => {
+  const response = await fetch(breakfastRouteUrl());
+  const breakfastList = await response.json();
+ 
+  breakfastList.forEach((breakfast, index) => { 
+    table.innerHTML +=  `
+      <tr class="listBreakfast" id="listBreakfast-${index}">
+        <td id="nome">
+          <input class="input-att" id="input-nome-${breakfast.id}" value=${breakfast.nome}>
+        </td>
+        <td id="cpf">
+          <input class="input-att" id="input-cpf-${breakfast.id}" value=${breakfast.cpf}>
+        </td>
+        <td id="mesa">
+          <input class="input-att" id="input-mesa-${breakfast.id}" value=${breakfast.mesa}>
+        </td>
+        <td>
+          <img src="./trash-can.png" id="${index}" onclick="deletar(${breakfast.id})" title="Excluir">
+          <img src="./system-update.png" id="${index}" onclick="atualizar(${breakfast.id})" title="Atualizar"> 
+        </td>
+      </tr>`
+  }); 
+};
+
+const renderizaForm = () => {
+  formulario.innerHTML = `
+    <input type="text" class="marge" id="nome" placeholder="Nome">
+    <input type="number" class="marge" id="cpf" placeholder="CPF">
+    <input type="text" class="marge" id="mesa" placeholder="Mesa café da manha">
+    <button type="button" class="marge" id="cad-button" value="Cadastrar" onclick="criar()">Cadastrar</button>
+    `
+};
+
+const criar = async () => {
+  const infos = {
+    nome: document.querySelector("#nome").value,
+    cpf: document.querySelector("#cpf").value,
+    mesa: document.querySelector("#mesa").value
+  };
+
+  if(infos.cpf.length < 11 || infos.cpf.length > 11){
+    alert("CPF deve conter 11 digitos, ex: 12345678900")
+    return false;
+  };
+
+  const response = await fetch(breakfastRouteUrl(), {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(infos)
+  });
+
+  if(response.status != 200){ 
+    alert("Erro ao criar.");
+  };
+
+  const content = await response.json();
+  renderizaForm();
+  reRenderizar();
+};
+
+const deletar = async (id) => {
+  await fetch(breakfastRouteUrl(), {
+    method: 'DELETE',
+    headers,
+    body: JSON.stringify({
+      id
+    })
+  })
+  .then(resposta => {
+    if(resposta.status === 200) {
+      reRenderizar();
+    }
+  });
+};
+
+const atualizar = async (id) => {
+  const infos = {
+    id,
+    nome: document.querySelector(`#input-nome-${id}`).value,
+    cpf: document.querySelector(`#input-cpf-${id}`).value,
+    mesa: document.querySelector(`#input-mesa-${id}`).value
+  };
+
+  if(infos.cpf.length < 11 || infos.cpf.length > 11){
+    alert("CPF deve conter 11 digitos, ex: 12345678900");
+    return false ;
+  }; 
+
+  const response = await fetch(breakfastRouteUrl(), {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(infos)
+  });
+  
+  if(response.status != 200){
+    alert("Erro ao alterar.");
+  };
+
+  const content = await response.json();
+  reRenderizar();
+}; 
+
+const reRenderizar = async() => {
+  table.innerHTML = initialTable.innerHTML;
+  await fetchBreakfast();
+};
+
+(() => {
+  fetchBreakfast();
+})();
